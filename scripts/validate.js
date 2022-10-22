12b6dc07bc919f5856f104bc9d887f414a7c123a
Daniil Davydovskiy <danildad.dad@yandex.ru>
2022-10-22 21:56:06 +0300

    fix: исправление вложенности файлов

--- scripts/validate.js
+++ scripts/validate.js
@@ -3,7 +3,7 @@ const settings = {
   inputSelector: '.popup__input',
   submitButtonSelector: '.popup__submit-button',
   inactiveButtonClass: 'popup__submit-button_disabled',
-  inputErrorClass: 'popup__input_type_error',
+  inputErrorClass: 'popup__input-error',
   errorClass: 'popup__error_visible',
 };
 
