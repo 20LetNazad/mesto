4221ae57295bc7e79cb629b048b29d6859fe57e9
Daniil Davydovskiy <danildad.dad@yandex.ru>
2022-12-04 19:16:25 +0300

    fix: объявить переменные с помощью this в PopupWithImage

--- src/scripts/PopupWithImage.js
+++ src/scripts/PopupWithImage.js
@@ -2,11 +2,11 @@ import Popup from './Popup.js';
 
 export default class PopupWithImage extends Popup {
   open(link, cardName) {
-    const imagePopupImg = document.querySelector('.image-popup__img');
-    const imagePopupImgDesc = document.querySelector('.image-popup__caption');
-    imagePopupImg.src = link;
-    imagePopupImg.alt = cardName;
-    imagePopupImgDesc.textContent = cardName;
+    this._imagePopupImg = document.querySelector('.image-popup__img');
+    this._imagePopupImgDesc = document.querySelector('.image-popup__caption');
+    this._imagePopupImg.src = link;
+    this._imagePopupImg.alt = cardName;
+    this._imagePopupImgDesc.textContent = cardName;
     super.open();
   }
 }
