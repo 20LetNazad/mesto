01fc47d6ed4fdf37c46caac8decba0df8b7f2e03
Daniil Davydovskiy <danildad.dad@yandex.ru>
2022-12-05 11:20:25 +0300

    fix: объявить переменные в конструкторе PopupWithImage

--- src/scripts/PopupWithImage.js
+++ src/scripts/PopupWithImage.js
@@ -1,9 +1,16 @@
 import Popup from './Popup.js';
 
 export default class PopupWithImage extends Popup {
+  constructor(popupSelector) {
+    super(popupSelector);
+    this._imagePopupImg =
+      this._popupSelector.querySelector('.image-popup__img');
+    this._imagePopupImgDesc = this._popupSelector.querySelector(
+      '.image-popup__caption'
+    );
+  }
+
   open(link, cardName) {
-    this._imagePopupImg = document.querySelector('.image-popup__img');
-    this._imagePopupImgDesc = document.querySelector('.image-popup__caption');
     this._imagePopupImg.src = link;
     this._imagePopupImg.alt = cardName;
     this._imagePopupImgDesc.textContent = cardName;
