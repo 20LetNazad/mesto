bdeb561b819fe00baed17775d470a3316afeda75
Daniil Davydovskiy <danildad.dad@yandex.ru>
2022-12-13 19:16:46 +0300

    feat: улучшить UX всех форм

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -6,6 +6,10 @@ export default class PopupWithForm extends Popup {
     this._formSubmit = formSubmit;
     this._popupForm = this._popupSelector.querySelector('.popup__form');
     this._inputList = document.querySelectorAll('.popup__input');
+    this._submitButton = this._popupSelector.querySelector(
+      '.popup__submit-button'
+    );
+    this._buttonTextContent = this._submitButton.textContent;
   }
 
   getInputValues() {
@@ -21,7 +25,13 @@ export default class PopupWithForm extends Popup {
     this._popupForm.addEventListener('submit', (evt) => {
       evt.preventDefault();
       this._formSubmit(this.getInputValues());
+      this._submitButton.textContent = `${this._buttonTextContent}...`;
     });
+  }
+
+  open() {
+    super.open();
+    this._submitButton.textContent = `${this._buttonTextContent}`;
   }
 
   close() {
